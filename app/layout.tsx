19d6fcda431fb2eee53eeb78ff2bf64d50912f51
oldroll1 <a9ckit22959@outlook.com>
2024-10-10 01:10:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7126448501409149"
+     crossorigin="anonymous"></script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><ThemeProvider
